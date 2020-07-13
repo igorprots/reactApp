04a371fc2536741182b916fc83ecc43aa0c45938
Igor Protsenko <protsxe51@gmail.com>
2020-07-13 19:05:00 +0300

    add prop-types

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,16 +3,30 @@ import TodoList from './toDo/TodoList';
 
 
 function App() {
-  const todos = [
-    { id: 1, completed: false, title: 'купить хлеб' },
-    { id: 2, completed: false, title: 'купить масло' },
-    { id: 3, completed: false, title: 'купить молоко' },
-  ]
+  const [todos, setTodos] = React.useState(
+    [
+      { id: 1, completed: false, title: 'купить хлеб' },
+      { id: 2, completed: false, title: 'купить масло' },
+      { id: 3, completed: false, title: 'купить молоко' },
+    ]
 
-  return( 
+  )
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+    )
+  }
+
+  return (
     <div className="wrapper">
       <h1>New Title</h1>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} onToggle={toggleTodo} />
     </div>
   )
 }
--- react-app/src/toDo/TodoItem.js
+++ react-app/src/toDo/TodoItem.js
@@ -1,10 +1,53 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function TodoItem({ todo, index }) {
+const styles = {
+    li: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        aliginItems: 'center',
+        padding: '.5rem 1rem',
+        border: '1px solid #ccc',
+        borderRadius: '4px',
+        marginBottom: '.5rem'
+    },
+    input: {
+        marginRight: '1rem'
+    }
+}
+
+function TodoItem({ todo, index, onChange }) {
+    const classes = []
+
+    if (todo.completed) {
+        classes.push("done")
+    }
+
+
     return (
-    <li>
-        <strong>{index + 1}</strong>
-        {todo.title}
-    </li>
+        <li style={styles.li}>
+            <span className={classes.join('')}>
+                <input
+                    type="checkbox"
+                    checked={todo.completed}
+                    style={styles.input}
+                    onChange={() => onChange(todo.id)}
+                />
+                <strong>{index + 1}</strong>
+                &nbsp;
+                {todo.title}
+            </span>
+            <button className='rm'>
+                &times;
+            </button>
+        </li >
     )
 }
+
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired,
+    index: PropTypes.number,
+    onChange: PropTypes.func.isRequired
+}
+
+export default TodoItem
